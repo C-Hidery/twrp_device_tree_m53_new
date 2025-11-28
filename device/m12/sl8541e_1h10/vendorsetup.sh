498ff7107ca649936df18d2a083cba04b40351df
CrepHidery <Ryan110413@outlook.com>
2025-11-29 00:20:09 +0800

    Update Files

--- device/m12/sl8541e_1h10/vendorsetup.sh
+++ device/m12/sl8541e_1h10/vendorsetup.sh
@@ -5,8 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),sl8541e_1h10)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
+add_lunch_combo omni_sl8541e_1h10-user
+add_lunch_combo omni_sl8541e_1h10-userdebug
+add_lunch_combo omni_sl8541e_1h10-eng
